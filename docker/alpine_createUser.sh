a3f1cbb6173bf95eb3c42fc90805def900c0f774
axelhenry <axel.durandil@gmail.com>
2017-12-12 12:14:01 +0100

    typo correction

--- docker/alpine_createUser.sh
+++ docker/alpine_createUser.sh
@@ -13,17 +13,17 @@ createUser () {
         echo "Groupname associated with given ($4) : ($_gid)."
         if [ -z "${_gid}" ]; then
             echo "Creating group ($3) with ($_gid) gid."
-            addgroup -g ${4} ${3} > /dev/null 2&>1
+            addgroup -g "${4}" "${3}" > /dev/null 2&>1
             _gid=$(grep ":$4:" /etc/group | cut -d: -f1)
         fi
 
         #   creating user
         echo "Creating user ($1) with uid ($2), gid ($_gid)."
-        adduser -S -G $_gid -u ${2} -H -D ${1}
+        adduser -S -G "${_gid}" -u "${2}" -H -D "${1}"
 
         #   if an optional function was passed to createUser, execute it with username and password as args
-        if [ -n "$6" ]; then
-            $6 $1 $2 $3 $4 $5
+        if [ -n "${6}" ]; then
+            "${6}" "${1}" "${2}" "${3}" "${4}" "${5}"
         fi
     fi
 }
